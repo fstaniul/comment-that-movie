cb72eea3c15ef8a9b07966200f47728b6e038e01
vip1all <vip.all@hotmail.com>
2018-07-12 23:18:56 +0200

    Created database connection and setup loading models. Added application start function

--- src/database.js
+++ src/database.js
@@ -1,1 +1,26 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
+const readdir = promisify(fs.readdir);
+
+const IS_DEV = (process.env.NODE_ENV || 'development') === 'development';
+
+// setup database connection and export Sequelize object
+const database = (module.exports = new Sequelize(process.env.DATABASE_URL || ':memory:', {
+  operatorsAliases: false,
+}));
+
+// Laod models stored in models folder
+const modelsLoadedPromise = readdir(path.join(__dirname, 'models')).then((modelFiles) => {
+  for (const modelFile of modelFiles) {
+    const name = modelFile.replace(/\.js/, '');
+    database.import(name, require(path.join(__dirname, 'models', modelFile)));
+  }
+});
+
+/**
+ * Connects to database, returns promise after successful connection.
+ */
+database.start = () => modelsLoadedPromise.then(() => database.sync({ force: IS_DEV }));
--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 const PORT = process.env.PORT || 3000;
 
 const express = require('express');
+const database = require('./database');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'Server created successfully!' }));
+app.get('/', (req, res) => {
+  res.send('Checkout the github repo for this REST Api: https://github.com/vip1all/comment-that-movie');
+});
 
-app.listen(PORT, () => console.log(`App listening on port :${PORT}`));
+app.start = async function() {
+  await database.start();
+  app.listen(PORT, () => {
+    cosnole.log(`Server started successfully, listening for connections on port :${PORT}`);
+  });
+};
